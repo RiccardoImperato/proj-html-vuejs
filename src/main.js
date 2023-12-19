0a26c76de6119a95bd32b693df99b91fe4a5c97c
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-19 15:02:34 +0100

    feat: font awsome e font open sans

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import '../src/assets/scss/style.scss'
 import App from './App.vue'
+/* Font Open sans */
+import "@fontsource/open-sans"
+/* import fontawesome*/
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-createApp(App).mount('#app')
+
+
+
+library.add(faClock, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
