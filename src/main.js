391947addb2dcbaffe439b092a820d27853d4422
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-19 19:33:42 +0100

    fix: font weight e aggiunta icone

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import '../src/assets/scss/style.scss'
 import App from './App.vue'
 /* Font Open sans */
-import "@fontsource/open-sans" // Defaults weight 400
-import "@fontsource/open-sans/800.css" // Bolder weight
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css"; // Bold
+import "@fontsource/open-sans/800.css"; // Bolder
 /* import fontawesome*/
 import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
@@ -14,7 +15,12 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { faChartPie } from '@fortawesome/free-solid-svg-icons'
+import { faGauge } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faClock, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin)
+
+
+library.add(faClock, faPhone, faEnvelope, faFacebook, faTwitter, faLinkedin, faUserGroup, faChartPie, faGauge)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
