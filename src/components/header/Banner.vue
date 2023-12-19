650117387e78d51ac3ba29b922152abc6639842b
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-19 13:12:35 +0100

    feat: struttura banner orari e contatti

--- src/components/header/Banner.vue
+++ src/components/header/Banner.vue
@@ -6,9 +6,41 @@ export default {
 </script>
 
 <template>
-    <div>
-        banner
+    <div class="container">
+        <div class="banner box">
+            <div>
+                i - Open Hours: Mon - Sat - 9:00 - 18:00
+            </div>
+            <ul>
+                <li>tel</li>
+                <li>e-mail</li>
+                <li>
+                    <ul class="socials">
+                        <li>i</li>
+                        <li>i</li>
+                        <li>i</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+.container {
+    color: #fff;
+    background-color: #24333d;
+
+    .banner {
+        padding: 20px 0;
+        display: flex;
+        justify-content: space-between;
+
+        ul {
+            list-style-type: none;
+            display: flex;
+            gap: 35px;
+        }
+    }
+}
+</style>
