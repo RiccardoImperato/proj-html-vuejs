1ef7452dad6f46853a4bee4e071d3a88adf1a67d
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-21 12:13:18 +0100

    fix: icone lista

--- src/components/main/List.vue
+++ src/components/main/List.vue
@@ -4,16 +4,20 @@ export default {}
 
 <template>
     <ul>
-        <li>> Standard Accounting</li>
-        <li>> Platfom Access</li>
-        <li>> Business Orientation</li>
-        <li>> Dedicated Consultant</li>
-        <li>> Personal Assistance</li>
+        <li><font-awesome-icon icon="fa-solid fa-chevron-right" /><span> Standard Accounting</span></li>
+        <li><font-awesome-icon icon="fa-solid fa-chevron-right" /> <span>Platfom Access</span></li>
+        <li><font-awesome-icon icon="fa-solid fa-chevron-right" /> <span>Business Orientation</span></li>
+        <li><font-awesome-icon icon="fa-solid fa-chevron-right" /> <span>Dedicated Consultant</span></li>
+        <li><font-awesome-icon icon="fa-solid fa-chevron-right" /> <span>Personal Assistance</span></li>
     </ul>
 </template>
 
 <style scoped lang="scss">
 li {
     margin: 15px 0;
+
+    span {
+        margin-left: 10px;
+    }
 }
 </style>
